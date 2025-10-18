547229c1f3a69d134e7ef8eac757541e5ae47219
Mason Carstens <ccmason@mac.dhcp.asu.edu>
2025-10-17 18:26:22 -0700

    Added user interface for functionality and greatestStoppingTime() function to calculate greatest stopping time from 1 to the given input

--- CollatzStoppingTime.py
+++ CollatzStoppingTime.py
@@ -1,19 +1,43 @@
 import sys
 
-def CollatzConjectureFunc(curNum, curCount):
+def collatzConjectureStoppingTime(curNum, curCount):
     if (curNum == 1):
         return curCount
     elif (curNum % 2 == 1):
-        return CollatzConjectureFunc((3 * curNum) + 1, curCount + 1)
+        return collatzConjectureStoppingTime((3 * curNum) + 1, curCount + 1)
     elif (curNum % 2 == 0):
-        return CollatzConjectureFunc(curNum / 2, curCount + 1)
+        return collatzConjectureStoppingTime(curNum / 2, curCount + 1)
     else:
         print("Critical Error: Exiting")
         return curCount
+    
+def greatestStoppingTime(inp):
+    curHighestNum = 1
+    curHighestMark = 0
+    for i in range(inp):
+        result = collatzConjectureStoppingTime(i + 1, 0)
+
+        if (result > curHighestMark):
+            curHighestMark = result
+            curHighestNum = i + 1
+
+    print("Collatz stopping time scanned from 1 to " + str(inp))
+    print("Num: " + str(curHighestNum))
+    print("Stopping Time: " + str(curHighestMark))
+    return [curHighestNum, curHighestMark]
+
 
 def main():
-    inp = int(sys.argv[1])
-    print("Number to test: " + str(inp))
-    print(CollatzConjectureFunc(inp, 0))
+    use = int(sys.argv[1])
+
+    if use == 1:
+        inp1 = int(sys.argv[2])
+        result = collatzConjectureStoppingTime(inp1, 0)
+        print("Stopping Time of " + str(inp1) + ": " + str(result))
+    elif use == 2:
+        inp1 = int(sys.argv[2])
+        greatestStoppingTime(inp1)
+    else:
+        print("Incorrect Program Usage: Invalid Use-Case Number (" + str(use) + ")")
 
 main()
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # CollatzStoppingTime
-Test program for pushing new versions of a program through Git/Github
+Overview:
+The following is a test respository to help learn Git/GitHub. The given project provides a program with helpful functions related to the stopping time of the Collatz Conjecture.
+
+Use:
+ ~: python3 CollatzStoppingTime.py [useNum] [input]
+ (Refer to useNum table below)
+ (Example: "~: python3 CollatzStoppingTime.py 2 100" calculates the highest stopping time from 1 to 100)
+
+ useNum:
+ 1: Calculate the stopping time of the given input
+ 2: Calculate the highest stopping time from 1 to the given input
+
+ # Created by Mason Carstens
